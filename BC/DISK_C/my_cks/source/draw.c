4ea4e2972367e925857291f0cf8d89dad834f6d3
xianyuh0016 <2496100220@qq.com>
2025-03-02 22:55:12 +0800

    登录框居中

--- BC/DISK_C/my_cks/source/draw.c
+++ BC/DISK_C/my_cks/source/draw.c
@@ -1,20 +1,24 @@
 #include <all_func.h>
+//屏幕宽度1024，高度768
+
 void draw_login()
 {
-	bar1(0, 0, 1024, 768,0xD5B1);
+    bar1(0, 0, 1024, 768,0xFFFF);
 
-	Fill_Rounded_Rectangle(200,220, 800, 630, 30,0xFFFF);
-	Draw_Rounded_Rectangle(200, 220, 800, 630, 30, 2,0xC618);//最外围灰色圆角矩形
-	
-	Draw_Rounded_Rectangle(300, 330, 700, 380, 5, 1,0xAEFC);//账号栏圆角矩形
-	Draw_Rounded_Rectangle(300, 410, 700, 460, 5, 1,0xAEFC);//密码栏圆角矩形
-	Fill_Rounded_Rectangle(300,490, 485, 540, 5,0x435c);//登录按钮
-	Draw_Rounded_Rectangle(515, 490, 700, 540, 5,1,0x0235);//注册按钮
-	PrintCC(305,250,"校园外卖快递平台",HEI,48,1,0);
-	PrintCC(305,345,"账号",HEI,24,1,0XC618);
-	PrintCC(305,425,"密码",HEI,24,1,0XC618);
-	PrintCC(365,503,"登录",HEI,24,1,0xFFFF);
-	PrintCC(580,503,"注册",HEI,24,1,0x0235);
-	
+    Fill_Rounded_Rectangle(200,180, 800, 630, 30,0xFFDF);
+    Draw_Rounded_Rectangle(200, 148, 824, 600, 30, 2,0x6B4D);//最外围灰色圆角矩形
+
+
+    
+    Draw_Rounded_Rectangle(300, 330, 700, 380, 5, 1,0xB71C);//账号栏圆角矩形
+    Draw_Rounded_Rectangle(300, 410, 700, 460, 5, 1,0xB71C);//密码栏圆角矩形
+    Fill_Rounded_Rectangle(300,490, 485, 540, 5,0x435c);//登录按钮
+    Draw_Rounded_Rectangle(515, 490, 700, 540, 5,1,0x0235);//注册按钮
+    PrintCC(305,225,"校园外卖快递平台",HEI,48,1,0);
+    PrintCC(305,345,"账号",HEI,24,1,0XC618);
+    PrintCC(305,425,"密码",HEI,24,1,0XC618);
+    PrintCC(365,503,"登录",HEI,24,1,0xFFFF);
+    PrintCC(580,503,"注册",HEI,24,1,0x0235);
+    
 }
 
