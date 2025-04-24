051beece1ccca4ea725b43259257e51ac6c9ccfd
xianyuh0016 <2496100220@qq.com>
2025-04-24 19:04:47 +0800

    合并成功能跑版

--- BC/DISK_C/my_cks/include/my.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MY_H
-#define __MY_H
-
-void my(int user_pos);
-void draw_my(USER currentUser);
-void press4(int x);
-#endif
--- BC/DISK_C/my_cks/include/ride.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RIDE_H__
-#define __RIDE_H__
-
-void rider(int user_pos);
-void draw_rider();
-void press3(int x);
-#endif
